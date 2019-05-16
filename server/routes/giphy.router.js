6c5bdd81b5bd0f6d8cf9976ec0ce17f8742864a8
Pete St. Martin <peter.r.stmartin@gmail.com>
2019-05-16 13:36:46 -0500

    minor edits

--- server/routes/giphy.router.js
+++ server/routes/giphy.router.js
@@ -6,7 +6,7 @@ const axios = require('axios');
 req('dotenv').config();
 
 router.get('/', (req, res) => {
-    axios.get(`http://api.giphy.com/v1/gifs/random?api_key=${process.env.GIPHY_API_KEY}`)
+    axios.get(`http://api.giphy.com/v1/gifs/random?api_key=${process.env.GIPHY_API_KEY}&tag=${req.query}`)
         .then(response => {
             console.log(response.data)
             res.send(response.data)
