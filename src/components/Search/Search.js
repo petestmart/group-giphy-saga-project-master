90e51b9bae3921b0830f2a6bb2821e1a1b393946
Vijayalakshmi Manoharan <viji@Vijayalakshmis-MacBook-Pro.local>
2019-05-16 11:58:54 -0500

    built search component

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,11 +1,37 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import {connect} from 'react-redux';
+
 
 class Search extends Component {
-    render(){
-        return(
-            <p>search list goes here</p>
+
+  state = {
+      keyword: ''
+  }
+  
+  handleChange = (event) => {
+      this.setState({
+          keyword : event.target.value
+      })
+  }
+
+  handleClick = () => {
+      this.props.dispatch({type:'SEARCH_IMAGES', payload:this.state.keyword});
+  } 
+
+    render() {
+        return (
+            <div>
+                <h2>SEARCH</h2>
+                <input type="text" placeholder="KEYWORD" onChange={this.handleChange} />
+                <button onClick={this.handleClick}>SEARCH</button>
+            </div>
         )
     }
 }
 
-export default Search;
+const mapRedux = reduxState => {
+    return {images: reduxState.pulledImages}
+}
+
+
+export default connect(mapRedux)(Search);
