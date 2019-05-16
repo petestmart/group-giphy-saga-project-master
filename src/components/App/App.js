6dc0f3dc43b0a2fd6c244a1a11ea471b8e86c13c
vijism89 <47097126+vijism89@users.noreply.github.com>
2019-05-16 13:15:59 -0500

    Revert "Feature viji"

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,4 @@
 import React, { Component } from 'react';
-import { HashRouter as Router, Route, Link } from 'react-router-dom';
-import Search from '../Search/Search';
-import Favorites from '../Favorites/Favorites';
-
 
 
 class App extends Component {
@@ -10,15 +6,11 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Router>
-          <h1>Giphy Search!</h1>
-          <Route exact path="/" component={Search}/>
-          <Route path="/favorites" component={Favorites}/>
-        </Router>
+        <h1>Giphy Search!</h1>
       </div>
     );
   }
-
+  
 }
 
 export default App;
