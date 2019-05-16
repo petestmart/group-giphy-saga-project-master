89cf1517e3c1d7fa9fa5be85774c7b5c7205c787
Vijayalakshmi Manoharan <viji@Vijayalakshmis-MacBook-Pro.local>
2019-05-16 11:39:32 -0500

    made the two main views

---
+++ src/components/Search/Search.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class Search extends Component {
+    render(){
+        return(
+            <p>search list goes here</p>
+        )
+    }
+}
+
+export default Search;
