ecc76aa41d79ef419d97005c556d4b272bc7b197
Pete St. Martin <peter.r.stmartin@gmail.com>
2019-05-17 10:34:39 -0500

    gifs appear on DOM. Partial functionality of favorites

--- server/routes/favorite.router.js
+++ server/routes/favorite.router.js
@@ -5,12 +5,28 @@ const router = express.Router();
 
 // return all favorite images
 router.get('/', (req, res) => {
-  res.sendStatus(200);
+  const queryText = `SELECT * FROM images`;
+  pool.query(queryText)
+    .then((result) => {
+      res.send(result.rows);
+    })
+    .catch((error) => {
+      console.log(`Error on query ${error}`);
+      res.sendStatus(500);
+    });
 });
 
 // add a new favorite 
 router.post('/', (req, res) => {
-  res.sendStatus(200);
+  console.log(`'sup bro?', ${ req.body.url }`)
+  const queryText = `INSERT INTO images (url) VALUES ('${req.body.url}')`;
+  pool.query(queryText)
+  .then((result) => {
+  res.sendStatus(201)})
+  .catch((error) => {
+    console.log(`Error on router.post favorites: ${error}`);
+    res.sendStatus(500);
+  })
 });
 
 // update given favorite with a category id
--- src/components/SearchItem/SearchItem.js
+++ src/components/SearchItem/SearchItem.js
@@ -4,13 +4,13 @@ import {connect} from 'react-redux';
 class SearchItem extends Component {
 
     handleClick = () => {
-        this.props.dispatch({type: 'SELECT_FAVORITE', payload: this.props.image.id});
+        this.props.dispatch({type: 'SELECT_FAVORITE', payload: this.props.image.images.original.url});
     }
 
     render() {
         return (
             <div>
-                <img src={this.props.image.image_url} alt={this.props.image.id} />
+                <img src={this.props.image.images.original.url} alt={this.props.image.id} />
                 <button onClick={this.handleClick}>FAVORITE</button>
             </div>
         )
